5d7c373af7992c3317cbc9501fcc8ba6e20a6a80
unknown <natetheman19@gmail.com>
2022-05-05 14:51:18 -0400

    Added clear button via DOM manipulation

--- draw.js
+++ draw.js
@@ -1,20 +1,30 @@
 function drawSquares(numSquares) {
     let sketchpadDiv = document.getElementById("sketchpad");
-
+    let clearBtn = document.createElement("button");
+    clearBtn.style.width = `100px`;
+    clearBtn.style.height = `20px`;
+    clearBtn.style["margin-top"] = `100px`;
+    clearBtn.innerHTML += "Clear grid";
+    
     for(let i=1; i<=4; i++){
         let squareRow = document.createElement("div");
         squareRow.className = "square-row";
         sketchpadDiv.appendChild(squareRow);
         for(let i=1; i<=4; i++){
             let square = document.createElement("div");
+            let squareClicked = false;
             square.className = "square-border";
-            square.addEventListener("mouseover", (e)=>{
-                console.log(e);
-                square.addEventListener("click", draw(square));
-            })
+            
+            square.addEventListener("click", ()=>{
+                squareClicked = true;
+                square.addEventListener("mouseover", (e)=>{
+                    if(squareClicked){draw(square)}
+                })
+            });
             squareRow.appendChild(square);
         }
     }
+    sketchpadDiv.appendChild(clearBtn);
 }
 
 let squares = document.getElementsByClassName("square-border");
